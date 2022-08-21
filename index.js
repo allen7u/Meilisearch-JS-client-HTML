c755cd8cccdfdd34877ff2ca6c4070245f19c52e
allen7u@GitKraken <allen7u@gmail.com>
2022-08-21 22:24:00 +0800

    add index picker, pagination, fixed layouts

--- index.js
+++ index.js
@@ -9,7 +9,40 @@
 
 
 
-var index
+// var index
+var index_id
+var offset
+var options
+var query
+var estimatedTotalHits 
+
+async function preQuery() {
+  
+    const client = new MeiliSearch({ host: 'http://127.0.0.1:7700' });
+  
+    var res = await client.getIndexes({ limit: 999 })
+    console.log(res)
+    for (let i in res.results) {
+        if (res.results.hasOwnProperty(i)) {
+            console.log(i,res.results[i])
+            console.log(i,res.results[i].uid)
+            let button = document.createElement('button')
+            button.textContent = res.results[i].uid
+            button.onclick = async () => {
+                console.log(index_id);
+                index_id = res.results[i].uid;
+                index = client.index(index_id);
+                var updated = await index.updateFilterableAttributes(["parent_id"]);
+                var p = document.createElement('p')
+                p.innerText = `${index_id}["parent_id"] updateFilterableAttributes done`
+                document.body.append(p);
+            }
+            var button_container = document.getElementById('index_selector_container')
+            button_container.append(button)
+        }
+    }
+  }
+preQuery()
 
 async function search( index_id, query, options ) {
     
@@ -18,7 +51,7 @@ async function search( index_id, query, options ) {
     });
     
     index = client.index( index_id );
-    // await index.updateFilterableAttributes([ "day_index"]);
+    // await index.updateFilterableAttributes([ "parent_id"]);
     var results = await index.search(
         query,
         options
@@ -33,7 +66,9 @@ function appendHitsToUl(results){
     results.hits.forEach( hit => {
         const li = document.createElement('li');
         li.classList.add('hit-item');
-        li.innerText = hit.content;
+        const content = hit._formatted.content;
+        const contentWithBreaks = content.replace(/__LINE_BREAK__/g, '<br>');
+        li.innerHTML = contentWithBreaks;
         ul.appendChild(li);
     } );
     var container = document.getElementById('hits');
@@ -64,21 +99,49 @@ function appendFacetsToUl(results){
 }
 
 document.querySelector("#search_input").addEventListener("input", async (e) => {
-    const query = e.target.value;
+    query = e.target.value;
     console.log(query);
-    const options = {
+    options = {
         limit:20,
         offset:0,
-        facets:['day_index','file_name']
+        attributesToHighlight: ["content"],
+        highlightPreTag: "<mark>",
+        highlightPostTag: "</mark>",
+        facets:['parent_id']
     };
-    const r = await search( "linely", query, options );
+    const r = await search( index_id, query, options );
     console.log(r);
-    for (var i = 0; i < r.hits.length; i++) {
-        console.log(r.hits[i]);
-    }
+    // for (var i = 0; i < r.hits.length; i++) {
+    //     console.log(r.hits[i]);
+    // }
+    estimatedTotalHits = r.estimatedTotalHits;
     appendHitsToUl(r);
-    appendFacetsToUl(r);
+    // appendFacetsToUl(r);
 }
+);
+
+// next page
+document.querySelector("#next_page").addEventListener("click", next_page
+);
+
+// previous page
+document.querySelector("#previous_page").addEventListener("click", previous_page
+);
+
+// bind right arrow key to next page
+document.addEventListener('keydown', function(e) {
+    if (e.key == 'ArrowRight' || e.key == 'f') {
+        next_page();
+        }
+    }
+);
+
+// bind left arrow key to previous page
+document.addEventListener('keydown', function(e) {
+    if (e.key == 'ArrowLeft' || e.key == 'd') {
+        previous_page();
+        }
+    }
 );
 
 // on document load
@@ -87,6 +150,31 @@ document.addEventListener('DOMContentLoaded', function() {
     randomDivBg();
     }
 );
+
+async function previous_page (e) {
+    if(options.offset - options.limit >= 0) {
+    options.offset -= options.limit;
+    }
+    const r = await search(index_id, query, options);
+    console.log(r);
+    // for (var i = 0; i < r.hits.length; i++) {
+    //     console.log(r.hits[i]);
+    // }
+    appendHitsToUl(r);
+};
+
+
+async function next_page (e) {
+    if(options.offset + options.limit < estimatedTotalHits) {
+        options.offset += options.limit;
+    }
+    const r = await search(index_id, query, options);
+    console.log(r);
+    // for (var i = 0; i < r.hits.length; i++) {
+    //     console.log(r.hits[i]);
+    // }
+    appendHitsToUl(r);
+};
 
 function getRandomColor() {
     var letters = 'BCDEF'.split('');
@@ -100,9 +188,9 @@ function getRandomColor() {
 function randomDivBg(){
 // console.log('from randomDivBg')
 document.querySelectorAll('div').forEach(function(div){
-    console.log(div)
-    console.log(div.style)
-    console.log(div.textContent)
+    // console.log(div)
+    // console.log(div.style)
+    // console.log(div.textContent)
     div.style.background = getRandomColor()
 })
 }
